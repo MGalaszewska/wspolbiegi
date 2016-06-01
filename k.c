07d818cc68ce78d97967fdcb2b758b919ba2a80d
MGalaszewska <marti18.1993@gmail.com>
2016-06-01 15:16:49 -0400

    Update k.c

--- k.c
+++ k.c
@@ -7,9 +7,16 @@
 #include <fcntl.h>
 #include <unistd.h>
 #include <pthread.h>
+#include <sys/ipc.h>
+#include <sys/shm.h>
+#include <sys/sem.h>
+#include <sys/stat.h>
 
 #define MINSIZE 60
 #define MAXSIZE 80
+#define SIZE 16
+#define shm_klucz 133
+#define klucz 13
 
 Display *mydisplay;
 Window mywindow;
@@ -45,8 +52,15 @@ int getrand(int min, int max) {
 	return (rand()%(max-min)+min);
 }
 
+//circle *circles;
 circle c;
-circle *circles;
+
+typedef struct dzielona {
+	circle *circles;
+}dzielona;
+int pamiec, semafor, sem_id;
+char* adres;
+dzielona *shared_var;
 
 pthread_t tid,tid1;
 pthread_mutex_t lock=PTHREAD_MUTEX_INITIALIZER;
@@ -95,7 +109,7 @@ void *reader(void *argum) {
 }
 
 
-int wyswietl() {
+int wyswietl(circle circles[SIZE]) {
    mydisplay = XOpenDisplay("");
    myscreen = DefaultScreen(mydisplay);
    myvisual = DefaultVisual(mydisplay,myscreen);
@@ -131,8 +145,8 @@ int wyswietl() {
    printf("Plik otwarty do zapisu\n");
    pthread_create(&tid,NULL,reader,(void *) &p);
    printf("Moj czytelnik utworzony\n");
-   pthread_create(&tid1,NULL,reader,NULL);
-   printf("Jego czytelnik utworzony\n");
+//   pthread_create(&tid1,NULL,reader,NULL);
+//   printf("Jego czytelnik utworzony\n");
    
    while (1)
    {
@@ -165,29 +179,12 @@ int wyswietl() {
 		  
          case Expose:
               XSetFunction(mydisplay,mygc,GXcopy);
-              amount = 16;
-              int x=0;
-              int y=0;
-              i=0;
-              circles = malloc(sizeof(circle)*amount);
-              while(i<amount) {
-				  for(j=0; j<4;j++) {
-					  for(k=0; k<4; k++) {
-						  int size = getrand(MINSIZE, MAXSIZE);
-						  circles[i].x = circles[i].xn = getrand(x+size/2, x+150-size);
-						  circles[i].y = circles[i].yn = getrand(y+size/2, y+150-size);
-						  circles[i].size = size;
-						  i++;
-						  x += 150;
-					  } y += 150; x=0;
-				  }
-			  }
 			  XSetForeground(mydisplay,mygc,mycolor.pixel);
-			  for(i=0; i<amount;i++) {
+			  for(i=0; i<SIZE;i++) {
 				  XFillArc(mydisplay, mywindow, mygc, circles[i].x-(circles[i].size/2), circles[i].y-(circles[i].size/2), circles[i].size, circles[i].size, 0, 360*64);
 			  }
 			  XSetForeground(mydisplay,mygc,mycolor1.pixel);
-			  for(i=0; i<amount; i++) {
+			  for(i=0; i<SIZE; i++) {
 				  char napis[2];
 				  sprintf(napis, "%d", i);
 				  XDrawString(mydisplay, mywindow, mygc, circles[i].xn, circles[i].yn, napis, strlen(napis));
@@ -199,6 +196,9 @@ int wyswietl() {
 			  case KeyPress:
 			  if(myevent.xkey.keycode == 0x09) {
 			  free(circles);
+			  semctl(semafor, 0, IPC_RMID, 0);
+			  shmdt(adres);
+			  shmctl(pamiec, IPC_RMID, 0);
               XCloseDisplay(mydisplay);
               close(fdw);
               remove("rysunek");
@@ -211,6 +211,64 @@ int wyswietl() {
 
 int main(int argc, char **argv) {
 srand(time(NULL));
-			wyswietl();
+pamiec = shmget(shm_klucz, sizeof(dzielona), 0777|IPC_CREAT);
+adres =shmat(pamiec, 0,0);
+shared_var = (dzielona*)adres;
+shared_var->circles = malloc(sizeof(circle)*SIZE);
+int player_id;
+FILE *coords, *coords2;
+int a, b, c, d, e;
+
+if((semafor = semget(klucz, 2, 0777|IPC_CREAT|IPC_EXCL)) != -1) {
+  coords = fopen("coords.txt", "w+");
+  int x=0;
+  int y=0;
+  i=0;
+	while(i<SIZE) {
+	  for(j=0; j<SIZE/4;j++) {
+		  for(k=0; k<SIZE/4; k++) {
+			  int size = getrand(MINSIZE, MAXSIZE);
+			  shared_var->circles[i].x = shared_var->circles[i].xn = getrand(x+size/2, x+150-size);
+			  fprintf(coords, "%d %d ", shared_var->circles[i].x, shared_var->circles[i].xn);
+			  shared_var->circles[i].y = shared_var->circles[i].yn = getrand(y+size/2, y+150-size);
+			  fprintf(coords, "%d %d ", shared_var->circles[i].y, shared_var->circles[i].yn);
+			  shared_var->circles[i].size = size;
+			  fprintf(coords, "%d ", shared_var->circles[i].size);
+			  i++;
+			  x += 150;
+		  } y += 150; x=0;
+	  }
+    }
+    fclose(coords);
+	player_id = 1;
+} else {
+	semafor = semget(klucz, 2, 0777|IPC_CREAT);
+	player_id = 2;
+	i=0;
+	coords2 = fopen("coords.txt", "r+");
+	//rewind(coords);
+	//while(feof(coords2)) {
+	while(i<SIZE) {
+		for(j=0; j<SIZE/4; j++) {
+			for(k=0; k<SIZE/4; k++) {
+				fscanf(coords2, "%d %d %d %d %d", &a, &b, &c, &d, &e);
+				shared_var->circles[i].x = a;
+				shared_var->circles[i].xn = b;
+				shared_var->circles[i].y = c;
+				shared_var->circles[i].yn = d;
+				shared_var->circles[i].size = e;
+				i++;
+			}
+		}
+	}
+//}
+	fclose(coords2);
+}
+semctl(semafor, 0, SETVAL, 1);
+semctl(semafor, 1, SETVAL, 1);
+for(i=0;i<SIZE; i++) {
+	printf("%d ", shared_var->circles[i].x);
+}
+wyswietl(shared_var->circles);
 	return 0;
 }
