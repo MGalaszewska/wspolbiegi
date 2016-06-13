14a224c11c495f568a1c691be9d27dfec21f9add
MGalaszewska <marti18.1993@gmail.com>
2016-06-13 11:09:28 -0400

    Update k.c

--- k.c
+++ k.c
@@ -27,12 +27,13 @@ Visual *myvisual;
 int mydepth;
 int myscreen;
 Colormap mycolormap;
-XColor mycolor,mycolor1, mycolor2, dummy;
+XColor mycolor,mycolor1, mycolor2, mycolor3, dummy;
 XEvent myevent;
 int p;
 char *myname;
 
 int i, j, k, amount;
+int sprawdzany;
 
 struct buffer { int x; int y; int pid; int prev; };
 int p;
@@ -40,33 +41,65 @@ int xw, yw, xw1, yw1;
 struct buffer *bufw;
 int bufsize=sizeof(struct buffer);
 
+typedef enum { false, true } bool;
+
+typedef struct player {
+	int player_id;
+	int points;
+} player;
+
 typedef struct circle {
 	int x;
 	int y;
 	int size;
 	int xn;
 	int yn;
+	int number;
+	bool odwiedzony;
+	int points;
 }circle;
+
+circle *circles;
 
 int getrand(int min, int max) {
 	return (rand()%(max-min)+min);
 }
 
-//circle *circles;
-circle c;
-
-typedef struct dzielona {
-	circle *circles;
-}dzielona;
-int pamiec, semafor, sem_id;
+int semafory, pamiec;
+int id;
 char* adres;
-dzielona *shared_var;
+const int SEM_ID = 9432;
 
 pthread_t tid,tid1;
 pthread_mutex_t lock=PTHREAD_MUTEX_INITIALIZER;
 
 void end() {
 	pthread_mutex_destroy(&lock);
+}
+struct sembuf bufor_zabierajacy_dostep1 = {0, -1, 0};
+struct sembuf bufor_dajacy_dostep1 = {0, 1, 0};
+struct sembuf bufor_zabierajacy_dostep2 = {1, -1, 0};
+struct sembuf bufor_dajacy_dostep2 = {1, 1, 0};
+
+int nr_pid;
+key_t klucz_pamieci;
+
+int sprawdz_czy_dotkniety(player gracz, int x, int y, circle *circles) {
+	for(i=0; i<SIZE; i++) {
+		if(x >= circles[i].x-circles[i].size && x <= circles[i].x+circles[i].size
+		&& y >= circles[i].y-circles[i].size && y <= circles[i].y+circles[i].size) {
+			circles[gracz.player_id].points+=1;
+			XSetForeground(mydisplay,mygc,mycolor3.pixel);
+			XFillArc(mydisplay, mywindow, mygc, circles[i].x-(circles[i].size/2), circles[i].y-(circles[i].size/2), circles[i].size, circles[i].size, 0, 360*64);
+			char napis[2];
+		    sprintf(napis, "%d", i);
+		    XSetForeground(mydisplay,mygc,mycolor1.pixel);
+		    XDrawString(mydisplay, mywindow, mygc, circles[i].xn, circles[i].yn, napis, strlen(napis));
+		    circles[i].number = i;
+			break;
+		}
+	}
+	return circles[gracz.player_id].points;
 }
 
 void *reader(void *argum) {
@@ -75,19 +108,16 @@ void *reader(void *argum) {
    int fd;
 
    buf=(struct buffer *) malloc(bufsize);
-//   printf("Zglasza sie reader\n");   
    while (!(fd=open("rysunek",O_RDONLY,0700)));
-   printf("Plik otwarty do odczytu\n");
+//   printf("Plik otwarty do odczytu\n");
    while (1) {  
       if (read(fd,buf,bufsize)>0) {
-         printf("Odczytano wspolrzedne: %5d, %5d\n",buf->x,buf->y);
-         printf("pid= %5d p= %5d argum= %8d\n",buf->pid,p,(int)argum);
+//         printf("Odczytano wspolrzedne: %5d, %5d\n",buf->x,buf->y);
+//         printf("pid= %5d p= %5d argum= %8d\n",buf->pid,p,(int)argum);
          if (((buf->pid==p)&&(argum!=NULL))||((buf->pid!=p)&&(argum==NULL))) {
-			printf("Probujemy zablokowac\n");
             pthread_mutex_lock(&lock);
             XSetForeground(mydisplay,mygc,mycolor2.pixel);
             XSetLineAttributes(mydisplay,mygc,1,LineSolid,CapProjecting,JoinMiter);
-			printf("KOLOR\n");
             if (buf->prev==0) {
                xr1=buf->x;
                yr1=buf->y;
@@ -108,8 +138,7 @@ void *reader(void *argum) {
    }
 }
 
-
-int wyswietl(circle circles[SIZE]) {
+int wyswietl(circle *circles, player gracz) {
    mydisplay = XOpenDisplay("");
    myscreen = DefaultScreen(mydisplay);
    myvisual = DefaultVisual(mydisplay,myscreen);
@@ -122,11 +151,12 @@ int wyswietl(circle circles[SIZE]) {
                             100,100,600,600,10,mydepth,InputOutput,
                             myvisual,CWBackingStore|CWBackingPlanes|CWBitGravity|
                             CWBackPixel|CWOverrideRedirect,&mywindowattributes);
-   XSelectInput(mydisplay,mywindow,ExposureMask|KeyPressMask|ButtonPressMask|ButtonMotionMask);
+   XSelectInput(mydisplay,mywindow,ExposureMask|KeyPressMask|KeyReleaseMask|ButtonPressMask|ButtonMotionMask|ButtonReleaseMask);
    mycolormap = DefaultColormap(mydisplay,myscreen);
          XAllocNamedColor(mydisplay,mycolormap,"blue",&mycolor,&dummy);
-         XAllocNamedColor(mydisplay,mycolormap,"red",&mycolor1,&dummy);
+         XAllocNamedColor(mydisplay,mycolormap,"green",&mycolor1,&dummy);
          XAllocNamedColor(mydisplay,mycolormap,"black",&mycolor2,&dummy);
+         XAllocNamedColor(mydisplay,mycolormap,"red",&mycolor3,&dummy);
          myname="Meh";
    XStoreName(mydisplay,mywindow,myname);
    XMapWindow(mydisplay,mywindow);
@@ -137,7 +167,6 @@ int wyswietl(circle circles[SIZE]) {
    struct buffer *bufw;
    
    bufsize=sizeof(struct buffer);
-   printf("Rozmiar bufora: %5d\n",bufsize);
    bufw=(struct buffer *) malloc(bufsize);
    p=getpid();
    
@@ -145,8 +174,8 @@ int wyswietl(circle circles[SIZE]) {
    printf("Plik otwarty do zapisu\n");
    pthread_create(&tid,NULL,reader,(void *) &p);
    printf("Moj czytelnik utworzony\n");
-//   pthread_create(&tid1,NULL,reader,NULL);
-//   printf("Jego czytelnik utworzony\n");
+   pthread_create(&tid1,NULL,reader,NULL);
+   printf("Jego czytelnik utworzony\n");
    
    while (1)
    {
@@ -156,18 +185,44 @@ int wyswietl(circle circles[SIZE]) {
 		  case ButtonPress:
               xw1=myevent.xbutton.x;
               yw1=myevent.xbutton.y;
-              printf("button: %3d  %3d\n",xw1,yw1);
+//              printf("button: %3d  %3d\n",xw1,yw1);
               bufw->x=xw1;
               bufw->y=yw1;
               bufw->prev=0;
               bufw->pid=p;
               write(fdw,bufw,bufsize);
               break;
+         
+         case ButtonRelease:
+			  xw1=myevent.xbutton.x;
+              yw1=myevent.xbutton.y;
+			  if(gracz.player_id == 1) {
+				if(semop(semafory, &bufor_dajacy_dostep2, 1) == -1) {
+					perror("Cos poszlo nie tak (operacja dajaca dostep1)\n");
+					exit(1);
+				}
+				if(semop(semafory, &bufor_zabierajacy_dostep1, 1) == -1) {
+					perror("Cos poszlo nie tak (operacja zabierajaca dostep1)\n");
+					exit(1);
+				}
+			}
+			else {
+				if(semop(semafory, &bufor_dajacy_dostep1, 1) == -1) {
+					perror("Cos poszlo nie tak (operacja dajaca dostep1)\n");
+					exit(1);
+				}
+				if(semop(semafory, &bufor_zabierajacy_dostep2, 1) == -1) {
+					perror("Cos poszlo nie tak (operacja zabierajaca dostep2)\n");
+					exit(1);
+				}
+			}
+			printf("%d: %d\n", gracz.player_id, sprawdz_czy_dotkniety(gracz, xw1, yw1, circles));
+			  break;
 
          case MotionNotify:
               xw=myevent.xmotion.x;
               yw=myevent.xmotion.y;
-              printf("motion: %3d  %3d\n",xw,yw);
+//              printf("motion: %3d  %3d\n",xw,yw);
               bufw->x=xw;
               bufw->y=yw;
               bufw->prev=1;
@@ -179,14 +234,21 @@ int wyswietl(circle circles[SIZE]) {
 		  
          case Expose:
               XSetFunction(mydisplay,mygc,GXcopy);
-			  XSetForeground(mydisplay,mygc,mycolor.pixel);
 			  for(i=0; i<SIZE;i++) {
-				  XFillArc(mydisplay, mywindow, mygc, circles[i].x-(circles[i].size/2), circles[i].y-(circles[i].size/2), circles[i].size, circles[i].size, 0, 360*64);
+				  if(i==0) {
+					circles[i].odwiedzony = true;
+					XSetForeground(mydisplay,mygc,mycolor3.pixel);
+					XFillArc(mydisplay, mywindow, mygc, circles[i].x-(circles[i].size/2), circles[i].y-(circles[i].size/2), circles[i].size, circles[i].size, 0, 360*64);
+				} else {
+					circles[i].odwiedzony = false;
+				    XSetForeground(mydisplay,mygc,mycolor.pixel);
+				    XFillArc(mydisplay, mywindow, mygc, circles[i].x-(circles[i].size/2), circles[i].y-(circles[i].size/2), circles[i].size, circles[i].size, 0, 360*64);
+			  }
 			  }
 			  XSetForeground(mydisplay,mygc,mycolor1.pixel);
 			  for(i=0; i<SIZE; i++) {
 				  char napis[2];
-				  sprintf(napis, "%d", i);
+				  sprintf(napis, "%d", circles[i].number);
 				  XDrawString(mydisplay, mywindow, mygc, circles[i].xn, circles[i].yn, napis, strlen(napis));
 			  }
               XSetForeground(mydisplay,mygc,mycolor2.pixel);
@@ -195,10 +257,9 @@ int wyswietl(circle circles[SIZE]) {
               
 			  case KeyPress:
 			  if(myevent.xkey.keycode == 0x09) {
-			  free(circles);
-			  semctl(semafor, 0, IPC_RMID, 0);
+			  semctl(semafory, 0, IPC_RMID, 0);
 			  shmdt(adres);
-			  shmctl(pamiec, IPC_RMID, 0);
+			  shmctl(pamiec, IPC_RMID, NULL);
               XCloseDisplay(mydisplay);
               close(fdw);
               remove("rysunek");
@@ -210,17 +271,39 @@ int wyswietl(circle circles[SIZE]) {
 }
 
 int main(int argc, char **argv) {
+	XInitThreads();
 srand(time(NULL));
-pamiec = shmget(shm_klucz, sizeof(dzielona), 0777|IPC_CREAT);
-adres =shmat(pamiec, 0,0);
-shared_var = (dzielona*)adres;
-shared_var->circles = malloc(sizeof(circle)*SIZE);
-int player_id;
-FILE *coords, *coords2;
-int a, b, c, d, e;
 
-if((semafor = semget(klucz, 2, 0777|IPC_CREAT|IPC_EXCL)) != -1) {
-  coords = fopen("coords.txt", "w+");
+player gracz;
+circle *adres;
+
+gracz.player_id = 1;
+gracz.points = 0;
+
+pamiec = shmget(SEM_ID, 1024, 0777|IPC_CREAT|IPC_EXCL);
+
+if(pamiec == -1){
+	gracz.player_id = 2;
+	gracz.points = 0;
+}	
+
+pamiec = shmget(SEM_ID, 1024, 0777|IPC_CREAT);
+adres = shmat(pamiec, 0, 0);
+semafory = semget(SEM_ID, 2, 0777|IPC_CREAT);
+if(semafory == -1) {
+	perror("Blad podczas tworzenia semaforow\n");
+	exit(1);
+}
+if(semctl(semafory, 0, SETVAL, (int)0) == -1) {
+	perror("Cos poszlo nie tak(semctl 1)\n");
+	exit(1);
+}
+if(semctl(semafory, 1, SETVAL, (int)0) == -1) {
+	perror("Cos poszlo nie tak(semctl 2)\n");
+	exit(1);
+}
+
+if(gracz.player_id == 1) {
   int x=0;
   int y=0;
   i=0;
@@ -228,47 +311,25 @@ if((semafor = semget(klucz, 2, 0777|IPC_CREAT|IPC_EXCL)) != -1) {
 	  for(j=0; j<SIZE/4;j++) {
 		  for(k=0; k<SIZE/4; k++) {
 			  int size = getrand(MINSIZE, MAXSIZE);
-			  shared_var->circles[i].x = shared_var->circles[i].xn = getrand(x+size/2, x+150-size);
-			  fprintf(coords, "%d %d ", shared_var->circles[i].x, shared_var->circles[i].xn);
-			  shared_var->circles[i].y = shared_var->circles[i].yn = getrand(y+size/2, y+150-size);
-			  fprintf(coords, "%d %d ", shared_var->circles[i].y, shared_var->circles[i].yn);
-			  shared_var->circles[i].size = size;
-			  fprintf(coords, "%d ", shared_var->circles[i].size);
+			  adres[i].x = adres[i].xn = getrand(x+size/2, x+150-size);
+			  adres[i].y = adres[i].yn = getrand(y+size/2, y+150-size);
+			  adres[i].size = size;
+			  adres[i].number = i;
+			  adres[i].points = 0;
 			  i++;
 			  x += 150;
 		  } y += 150; x=0;
 	  }
     }
-    fclose(coords);
-	player_id = 1;
-} else {
-	semafor = semget(klucz, 2, 0777|IPC_CREAT);
-	player_id = 2;
-	i=0;
-	coords2 = fopen("coords.txt", "r+");
-	//rewind(coords);
-	//while(feof(coords2)) {
-	while(i<SIZE) {
-		for(j=0; j<SIZE/4; j++) {
-			for(k=0; k<SIZE/4; k++) {
-				fscanf(coords2, "%d %d %d %d %d", &a, &b, &c, &d, &e);
-				shared_var->circles[i].x = a;
-				shared_var->circles[i].xn = b;
-				shared_var->circles[i].y = c;
-				shared_var->circles[i].yn = d;
-				shared_var->circles[i].size = e;
-				i++;
-			}
-		}
-	}
-//}
-	fclose(coords2);
+	wyswietl(adres, gracz);
 }
-semctl(semafor, 0, SETVAL, 1);
-semctl(semafor, 1, SETVAL, 1);
-for(i=0;i<SIZE; i++) {
-	printf("%d ", shared_var->circles[i].x);
+else {
+	wyswietl(adres, gracz);
 }
-wyswietl(shared_var->circles);
+
+if(semctl(semafory, 1, IPC_RMID, 0) == -1) {
+	perror("Cos poszlo nie tak (semctl2)\n");
+	exit(1);
+}
 	return 0;
 }
